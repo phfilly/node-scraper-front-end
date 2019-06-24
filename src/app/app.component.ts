7979dd7e38d415b857de31beef9da192a916f40f
phfilly <philipdutoit@yahoo.com>
2019-06-24 08:38:09 +0200

    sorting items
    cleaning up price

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,50 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class AppComponent implements OnInit {
   title = 'scraper-sama';
   loading = true;
+  message = '';
   items = [];
 
-  constructor(public db: AngularFirestore) {
-  }
+  constructor(public db: AngularFirestore) { }
 
   async ngOnInit() {
     await this.getItems()
     .subscribe(result => {
-      this.items = result.map((x) => x.payload.doc.data());
+      
+      if (result.length > 0) {
+        this.items = result
+        .map((x) => x.payload.doc.data())
+        .map((item) => {
+          return {
+            ...item,
+            dateTime: this.convertEpocToTime(item['date']),
+            price: this.priceConvert(item['price'])
+          };
+        })
+        .sort((a, b) => a['price'] - b['price']);
+      } else {
+        this.message = 'No products found...';
+      }
       this.loading = false;
-      // console.log(result[0].payload.doc.data());
       console.log(this.items);
     });
   }
 
   getItems() {
     return this.db.collection('laptops').snapshotChanges();
+  }
+
+  priceConvert(item: string) {
+    if (item.includes(' X ')) {
+      const tmpPrice = item.split('X');
+      const totalPayment = parseInt(tmpPrice[0].trim(), 10) * parseInt(tmpPrice[1].trim(), 10);
+      return totalPayment.toFixed(2);
+    } else {
+      return parseInt(item, 10);
+    }
+  }
+
+  convertEpocToTime(epoch: number) {
+    const newDate = new Date(epoch);
+    return `${newDate.toLocaleString()}`;
   }
 }
